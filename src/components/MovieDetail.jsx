a63b116d7c572d1c8a5430ea5324d0c839fcc379
Stefano Casasola <ste.casasola@gmail.com>
2022-09-05 12:59:26 +0200

    Completa il progetto
    Completa il progetto inserendo la condizione corretta in componentDidUpdate

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Spinner } from 'react-bootstrap'
 
 class MovieDetail extends Component {
   state = {
@@ -12,11 +12,31 @@ class MovieDetail extends Component {
     this.fetchMovieDetails()
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    // questo metodo intercetta qualsiasi aggiornamento nel componente
+    // quindi ogni cambio di stato e ogni cambio di props
+    console.log('COMPONENTDIDUPDATE')
+
+    // prevProps e prevState sono i due parametri di componentDidUpdate
+    // prevProps è la versione precedente di this.props
+    // prevState è la versione precedente di this.state
+    //  nel nostro caso, noi vogliamo che fetchMovieDetails() venga invocato
+    // quando scegliamo un nuovo titolo <-- quindi quando c'è un cambio nelle props
+    // quello che NON vogliamo è che venga invocato di nuovo a seguito del
+    // setState
+    if (prevProps.selectedMovie !== this.props.selectedMovie) {
+      // UNA CONDIZIONE DEL GENERE È OBBLIGATORIA IN OGNI COMPONENTDIDUPDATE
+      this.fetchMovieDetails()
+      // la condizione è necessaria in componentDidUpdate per fare in modo di
+      // non entrare in un loop infinito, che è il pericolo n.1 di questo metodo
+    }
+  }
+
   fetchMovieDetails = async () => {
     try {
       let response = await fetch(
         'http://www.omdbapi.com/?apikey=24ad60e9&s=' + this.props.selectedMovie
-      )
+      ) // this.props.selectedMovie inizialmente sarà sempre Iron Man
       console.log('RESPONSE: ', response)
       if (response.ok) {
         // la chiamata è andata a buon fine
@@ -25,7 +45,7 @@ class MovieDetail extends Component {
         console.log(data)
         this.setState({
           movie: data.Search[0], // <-- il mio risultato corretto
-        })
+        }) // dopo setState parte SEMPRE render() e, se c'è, componentDidUpdate
       } else {
         alert('ERRORE NEL RECUPERO DEL FILM')
       }
@@ -36,6 +56,11 @@ class MovieDetail extends Component {
 
   render() {
     console.log('sono render')
+    // ogni cambio nelle prop o nello state provoca una nuova
+    // invocazione di render()
+    // this.fetchMovieDetails() // <-- SBAGLIATO, perchè fetchMovieDetails()
+    // effettua un setState! e ad ogni cambio di stato render() di invoca di nuovo
+    // --> INFINITE LOOP
     return (
       <>
         {this.state.movie ? (
@@ -49,7 +74,9 @@ class MovieDetail extends Component {
             </Card.Body>
           </Card>
         ) : (
-          <div>LOADING...</div>
+          <div>
+            <Spinner animation="border" variant="warning" />
+          </div>
         )}
       </>
     )
